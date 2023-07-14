de14662900cc27926aee6795d08cee854e7086c6
Maheshwar-S <138221756+Maheshwar-S@users.noreply.github.com>
2023-07-14 09:29:35 +0530

    Update ai assisstant.py

--- ai assisstant.py
+++ ai assisstant.py
@@ -8,8 +8,7 @@ import smtplib
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-# print(voices[1].id)
-engine.setProperty('voice', voices[0].id)
+engine.setProperty('voice', voices[1].id) #voice[1] for Zira and voice[0] for Hazel
 
 
 def speak(audio):
